545110918488e00e042192e2659d654368cb209e
senik11 <mailsenik11@gmail.com>
2017-03-10 20:25:24 +0300

    1

---
+++ src/main/java/edu/androidclub/servletexample1/App.java
@@ -0,0 +1,30 @@
+package edu.androidclub.servletexample1;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+@ApplicationPath("/")
+public class App extends Application {
+
+    private Set<Class<?>> classes = new HashSet<>();
+    private Set<Object> singletones = new HashSet<>();
+
+    public App() {
+        classes.add(JacksonJaxbJsonProvider.class);
+        singletones.add(new TestResource());
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public Set<Object> getSingletons() {
+        return singletones;
+    }
+}
